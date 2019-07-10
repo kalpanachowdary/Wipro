7a693ff50bad772ef1b1a5e061f601d523acbc84
hp <hp@mypc>
2019-07-10 15:09:54 +0530

    commit done

---
+++ github.com/kalpanachowdary/Wipro.git/Wipro/src/w1.java
@@ -0,0 +1,7 @@
+public class w1 {
+	public static void main(String[] args) {
+		System.out.println(args[0]+" "+"technologies"+" "+args[1]);
+		System.out.println(args[2]+" "+"tecnologies"+" "+args[3]);
+	}
+
+}
