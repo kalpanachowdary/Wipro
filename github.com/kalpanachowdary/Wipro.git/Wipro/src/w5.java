a617de23a9113501790382ddb7c61ad147fa8add
hp <hp@mypc>
2019-07-12 15:48:01 +0530

    commit done

---
+++ github.com/kalpanachowdary/Wipro.git/Wipro/src/w5.java
@@ -0,0 +1,11 @@
+
+public class w5 {
+	public static void main(String[] args) {
+		int int1,int2,sum;
+		int1=Integer.parseInt(args[0]);
+		int2=Integer.parseInt(args[1]);
+		sum=int1+int2;
+		System.out.println("The sum of int1 and int2 is"+" "+sum);
+	}
+
+}
