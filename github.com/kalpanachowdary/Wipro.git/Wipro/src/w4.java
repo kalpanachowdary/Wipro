37c405ffbe8c4842c4316d7c60560934513f568f
hp <hp@mypc>
2019-07-12 14:54:36 +0530

    commit done

--- github.com/kalpanachowdary/Wipro.git/Wipro/src/w4.java
+++ github.com/kalpanachowdary/Wipro.git/Wipro/src/w4.java
@@ -1,4 +1,13 @@
 
 public class w4 {
+	public static void main (String[] args) {
+		for(int i=0;i<=120;i++)
+		{
+			if(i%2==0&&i%3==0&&i%5==0)
+			{
+				System.out.println(i);			
+			}
+		}
+	}
 
 }
